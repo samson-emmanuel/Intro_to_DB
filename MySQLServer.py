b595fb32f4c797337587df8910212e65c8c98553
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-12-14 23:55:23 +0100

    updated code added

--- MySQLServer.py
+++ MySQLServer.py
@@ -21,7 +21,7 @@ def create_database():
 
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
