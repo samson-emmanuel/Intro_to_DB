11b9cac8c5cf984254727979b6d1eac9abd207d8
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-12-15 00:01:10 +0100

    updating syntax

--- task_2.sql
+++ task_2.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
-    quantity INT NOT NULL,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
